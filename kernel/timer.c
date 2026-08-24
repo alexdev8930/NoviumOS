108469eb49252267361edc04d8b3ee72380ca6e5
alexdev8930 <alexdev8930@proton.me>
2026-08-24 11:19:12 -0700

    hw: added PIT constants and a timer_callback() function

--- kernel/timer.c
+++ kernel/timer.c
@@ -1,1 +1,15 @@
-// TODO: System stub - implement when ready.
+#include <kernel/timer.h>                                 
+#include <kernel/irq.h>                         
+#include <asm/io.h>
+
+#define PIT_OSC_FREQ  1193182u                            
+#define PIT_CH0_DATA  0x40                             
+#define PIT_CMD_REG   0x43             
+
+static volatile u32 tick_count = 0;
+static u32 ticks_per_second = 0;
+
+static void timer_callback(struct registers *regs) {
+    (void)regs;
+    tick_count++;
+}
--- kernel/timer.h
+++ kernel/timer.h
@@ -1,1 +1,11 @@
-// TODO: System stub - implement when ready.
+#ifndef NOVIUM_TIMER_H                                     
+#define NOVIUM_TIMER_H                                   
+
+#include <novium/types.h>                                 
+
+void timer_init(int hz);                                   
+u32  timer_get_ticks(void);                              
+u64  timer_uptime_ms(void);                               
+void timer_sleep_ms(u32 ms);                             
+
+#endif                                                   
