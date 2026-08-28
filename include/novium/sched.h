e78bcf2947f02c51a4dab2a62918f53f06e087bb
dudas131 <dudzproductions101@gmail.com>
2026-08-28 22:46:04 +0000

    Update sched.h

--- include/novium/sched.h
+++ include/novium/sched.h
@@ -1,1 +1,9 @@
-// TODO: System stub - implement when ready.
+#ifndef NOVIUM_DEBUG_H
+#define NOVIUM_DEBUG_H
+
+#include "types.h"
+
+void debug_print_hex8(u8 v);
+void debug_print_hex32(u32 v);
+
+#endif
